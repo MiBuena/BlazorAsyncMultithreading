b9446448b4357dd5bfc6e60bf0c588d7d84907d2
MiBuena <athina.palada@gmail.com>
2021-12-20 13:40:03 +0200

    Add repository for Ethnicity

--- PopulationCensus/PopulationCensus.Data/UnitOfWork/UnitOfWork.cs
+++ PopulationCensus/PopulationCensus.Data/UnitOfWork/UnitOfWork.cs
@@ -14,19 +14,24 @@ namespace PopulationCensus.Data.UnitOfWork
         private readonly PopulationContext _context;
         private readonly IAsyncRepository<Age> _agesRepository;
         private readonly IAsyncRepository<Area> _areasRepository;
+        private readonly IAsyncRepository<Ethnicity> _ethnicitiesRepository;
 
         public UnitOfWork(PopulationContext context,
             IAsyncRepository<Age> agesRepository,
-            IAsyncRepository<Area> areasRepository)
+            IAsyncRepository<Area> areasRepository,
+            IAsyncRepository<Ethnicity> ethnicityRepository)
         {
             _context = context;
             _agesRepository = agesRepository;
             _areasRepository = areasRepository;
+            _ethnicitiesRepository = ethnicityRepository;
         }
 
         public IAsyncRepository<Age> AgesRepository => this._agesRepository;
 
         public IAsyncRepository<Area> AreasRepository => this._areasRepository;
+
+        public IAsyncRepository<Ethnicity> EthnicitiesRepository => this._ethnicitiesRepository;
 
         public Task<int> SaveChangesAsync() => this._context.SaveChangesAsync();
     }
