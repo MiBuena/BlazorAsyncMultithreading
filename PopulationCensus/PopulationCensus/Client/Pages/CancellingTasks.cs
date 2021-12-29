fb94ed5c3f8ebabe3d7ec3906e856b9d2fbf362d
MiBuena <athina.palada@gmail.com>
2021-12-29 18:30:46 +0200

    Add CancellingTasks razor component

---
+++ PopulationCensus/PopulationCensus/Client/Pages/CancellingTasks.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PopulationCensus.Client.Pages
+{
+    public partial class CancellingTasks
+    {
+        [Inject]
+        private HttpClient _httpClient { get; set; }
+
+        public async void UploadLargeFile()
+        {
+            await ReadLargeFileAsync("Files/First10lines.csv");
+        }
+
+
+        public async Task<LinkedList<string>> ReadLargeFileAsync(string path)
+        {
+            var lines = new LinkedList<string>();
+
+            using (var stream = await _httpClient.GetStreamAsync(path))
+            {
+                using (BufferedStream bs = new BufferedStream(stream))
+                { 
+                    using (StreamReader sr = new StreamReader(bs))
+                    {
+                        await sr.ReadLineAsync();
+
+                        string? line;
+                        while ((line = await sr.ReadLineAsync()) != null)
+                        {
+                            lines.AddLast(line);
+                        }
+                    }
+                }
+
+            }
+
+            await Task.Delay(5000);
+            return lines;
+        }
+    }
+}
